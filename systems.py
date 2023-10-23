07ed408b0451340ae34c6b41bd1870573f1e628b
Adrian Müller <dev@adrianmueller.eu>
2023-10-24 02:11:13 +0300

    systems.py: Add phase diagram and bifurcation diagram for 1d

--- systems.py
+++ systems.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp  # used for numerical integration
-from itertools import product
+from scipy.optimize import fsolve
 
 ############
 ### Flow ###
@@ -64,6 +64,61 @@ def ODE_flow_2d(f, x0s=None, xlim=(-2,2), ylim=(-2,2), T=10, n_timesteps=100, ax
 ### Phase portrait ###
 ######################
 
+def is_stable(f, fp, T=100, dt=1, eps=1e-3, verbose=False):
+    fp = np.array(fp)
+    dims = len(fp)
+    noise = (np.random.random(dims) - 0.5)*2*eps
+    #noise /= np.sum(noise**2) # normalize to length one = move to random direction
+    x = simulate(f, fp+noise, T, dt)
+    # get the last value of the simulation
+    res = [x[i][-1] for i in range(dims)]
+    error = np.sum((res - fp)**2)
+    stable = error < eps # stable if it remains inside eps
+    if verbose:
+        print(f"Fixed point {fp}: {stable} (eps: {error})") # init: {fp + noise}, res: {res},
+    return stable
+
+def ODE_phase_1d(f, x_limits=(-2,2), T=10, n_timesteps=100, ax=None, n_arrows=20,
+                 fp_resolution=100, fp_eps=2.5e-3, fp_stability_eps=1e-2):
+    x_min, x_max = x_limits
+    dt = T/n_timesteps
+    dx = (x_max - x_min)/(fp_resolution*n_arrows)
+    x = np.arange(x_min, x_max, dx)
+    x_dot = f(x)
+
+    # plot the function
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(x, x_dot)
+
+    # fixed points
+    eps = fp_eps
+    fps = np.where(np.abs(x_dot) < eps)
+    print(fps, x_dot.shape, x.shape)
+    fps = x[fps]
+    for fp in fps:
+        if is_stable(f, [fp], T, dt, fp_stability_eps, verbose=True):
+            plt.scatter(fp, 0, facecolors='k', edgecolors='k')
+        else:
+            plt.scatter(fp, 0, facecolors='none', edgecolors='r')
+
+    # the slope field
+    skip = fp_resolution
+    x_dot_ = x_dot[::skip]
+    x_ = x[::skip]
+    print(x.shape, x_.shape, x_dot.shape, x_dot_.shape)
+    q = ax.quiver(x_, np.zeros_like(x_), x_dot_, np.zeros_like(x_dot_), np.abs(x_dot_),
+                  cmap='jet', pivot='mid', angles='xy', headwidth=3, headlength=1, headaxislength=1)
+    ax.quiverkey(q, X=0.8, Y=1.03, U=2, label='dx/dt', labelpos='E')
+    ax.set_xlim(*ax.get_xlim())
+    ax.set_ylim(*ax.get_ylim())
+
+    ax.set_title('Phase portrait')
+    ax.set_xlabel('x')
+    ax.set_ylabel('$\dot{x}$')
+    ax.grid()
+    plt.show()
+
 def ODE_phase_2d(f, x0s=None, xlim=(-2,2), ylim=(-2,2), T=10, n_timesteps=100, ax=None, x_arrows=20, y_arrows=20,
               nullclines=False, nullclines_eps=5e-4, fp_resolution=100, fp_eps=2.5e-3, fp_stability_eps=1e-2):
 
@@ -90,17 +145,6 @@ def ODE_phase_2d(f, x0s=None, xlim=(-2,2), ylim=(-2,2), T=10, n_timesteps=100, a
             fps.add(tuple(fp))
 
         return fps
-
-    def is_stable(f, fp, T, dt, eps=1e-2):
-        fp = np.array(fp)
-        noise = np.random.random(2)*eps
-        #noise /= np.sum(noise**2) # normalize to length one = move to random direction
-        x,y,_ = simulate(f, fp+noise, T, dt)
-        res = [x[-1], y[-1]]
-        error = np.sum((res - fp)**2)
-        stable = error < eps # stable if it remains inside eps
-        print(f"Fixed point {fp}: {stable} (eps: {error})") # init: {fp + noise}, res: {res},
-        return stable
 
     x_min, x_max = xlim
     y_min, y_max = ylim
@@ -145,7 +189,7 @@ def ODE_phase_2d(f, x0s=None, xlim=(-2,2), ylim=(-2,2), T=10, n_timesteps=100, a
     eps = fp_eps
     fps = find_fixed_points(x_dot, y_dot, eps, x_min, dx, y_min, dy)
     for fp in fps:
-        if is_stable(f, fp, T, dt, fp_stability_eps):
+        if is_stable(f, fp, T, dt, fp_stability_eps, verbose=True):
             plt.scatter(*fp, facecolors='k', edgecolors='k')
         else:
             plt.scatter(*fp, facecolors='none', edgecolors='k')
@@ -184,3 +228,46 @@ def ODE_phase_2d_polar(f, polar0s=None, x0s=None, rlim=2, **args):
 
     return ODE_phase_2d(f_cartesian, x0s, xlim=(-rlim, rlim), ylim=(-rlim, rlim), **args)
 
+###########################
+### Bifurcation diagram ###
+###########################
+
+def get_roots(f, x0s, prec=1e-5):
+    roots = set()
+    for x0 in x0s:
+        cand = fsolve(f, x0)
+        if np.linalg.norm(f(cand)) < prec:
+            cand = np.round(cand, -int(np.log10(prec)))
+            roots.add(tuple(cand))
+    return list(roots)
+
+def bifurcation_diagram_1d(f, x0, r_range, dr=None, prec=1e-5, plot=True, stability=True):
+    # get roots for each r
+    if dr is None:
+        dr = (r_range[1]-r_range[0])/200
+    rs = np.arange(*r_range, dr)
+    all_roots = []
+    for r in rs:
+        roots = get_roots(lambda x: f(x, r), x0, prec)
+        # print(r, roots)
+        all_roots.append(roots)
+
+    # stability analysis
+    if stability:
+        stabilities = []
+        for r, roots in zip(rs, all_roots):
+            stabilities.append([is_stable(lambda x: f(x, r), root) for root in roots])
+
+    if plot:
+        plt.figure(figsize=(10, 6))
+        # scatter stable roots as black dots and unstable roots as red dots
+        for r, roots, stable in zip(rs, all_roots, stabilities):
+            for root in roots:
+                color = 'k' if stable[roots.index(root)] else 'r'
+                plt.scatter(r, root, c=color, s=2)
+        plt.title('Bifurcation diagram')
+        plt.xlabel('r')
+        plt.ylabel('x')
+        plt.xlim(r_range)
+        plt.grid()
+    return rs, all_roots
