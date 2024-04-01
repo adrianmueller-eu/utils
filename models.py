8b97795eeb2e543f88deb346e9f31e70e6f1cc6b
Adrian Müller <dev@adrianmueller.eu>
2024-04-02 00:30:50 +0300

    models: Merge label and __str__, make precision adjustable, and rename fit_err -> error

--- models.py
+++ models.py
@@ -6,8 +6,12 @@ from scipy.optimize import curve_fit
 # convenience functions
 def pm(x, y=None, deg=1, plot=True):
     if y is None:
-        y = x
-        x = np.arange(len(y))
+        if 1950 < x[0] < 2050 and all(1950 < x_i < 2050 for x_i in x[::2]):
+            y = x[1::2]
+            x = x[::2]
+        else:
+            y = x
+            x = np.arange(len(y))
     if deg >= 0:
         poly = Polynomial.fit(x, y, deg)
     else:
@@ -40,25 +44,24 @@ def expm(x, y=None, plot=True):
 # arm
 
 # helper methods
-def _generate_poly_label(coeff):
+def _generate_poly_label(coeff, precision=3):
     q = len(coeff) - 1
     res = ""
     for i,c in enumerate(reversed(coeff)):
        if q-i == 0:
-          res += "%.3f" % c
+          res += f"%.{precision}f" % c
        else:
-          res += "%.3fx^%d + " % (c, q-i)
+          res +=f"%.{precision}fx^%d + " % (c, q-i)
     return res
 
 # Functions
 class Function(ABC):
     """
-    Abstract class for functions. It provides an MSE error function and a plot method.
+    Abstract class for functions. It provides an MSE error function and a plotting method.
     Each descendant has to implement the following methods:
     - fit(x, y)
     - __call__(x)
     - __str__()
-    - _plot_label()
     """
 
     @staticmethod
@@ -66,7 +69,7 @@ class Function(ABC):
     def fit(x, y):
         pass
 
-    def error(self, x, y):
+    def mse(self, x, y):
         return np.mean(np.abs(self(x) - y)**2)
 
     @abstractmethod
@@ -74,25 +77,25 @@ class Function(ABC):
         pass
 
     @abstractmethod
-    def __str__(self):
+    def __str__(self, precision=3):
         pass
 
     def __repr__(self):
         return self.__str__()
 
-    @abstractmethod
-    def _plot_label(self):
-        pass
+    def label(self, precision=3, show_error=True):
+        if show_error and hasattr(self, "error"):
+            return self.__str__(precision) + f" (MSE: {self.error:.{precision}f})"
+        return self.__str__(precision)
 
-    def plot(self, x, ax=None):
+    def plot(self, x, ax=None, precision=3):
         import matplotlib.pyplot as plt
 
         if ax is None:
             ax = plt.gca()
-        ax.plot(x, self(x), label=self._plot_label())
+        ax.plot(x, self(x), label=self.label(precision))
         ax.legend()
         return ax
-
 
 class Polynomial(Function):
     """ y = a_n*x^n + ... + a_1*x + a_0 """
@@ -110,17 +113,14 @@ class Polynomial(Function):
         y = np.array(list(y))
         coeff = polyfit(x, y, deg)
         p = Polynomial(coeff)
-        p.fit_err = p.error(x,y)
+        p.error = p.mse(x,y)
         return p
 
     def __call__(self, x):
         return polyval(np.array(x), self.coeff)
 
-    def __str__(self):
-        return f"Polynomial of degree %d with coeff %s" % (self.degree, self.coeff)
-
-    def _plot_label(self):
-        return _generate_poly_label(self.coeff)
+    def __str__(self, precision=3):
+        return _generate_poly_label(self.coeff, precision)
 
 class InversePolynomial(Function):
     """ y = 1/(a_n*x^n + ... + a_1*x + a_0) """
@@ -138,17 +138,14 @@ class InversePolynomial(Function):
         y = np.array(list(y))
         coeff = polyfit(x, 1/y, -deg)
         p = InversePolynomial(coeff)
-        p.fit_err = p.error(x,y)
+        p.error = p.mse(x,y)
         return p
 
     def __call__(self, x):
         return 1/polyval(np.array(x), self.coeff)
 
-    def __str__(self):
-        return f"Inverse polynomial of degree %d with coeff %s" % (self.degree, self.coeff)
-
-    def _plot_label(self):
-        return "1/(" + _generate_poly_label(self.coeff) + ")"
+    def __str__(self, precision=3):
+        "1/(" + _generate_poly_label(self.coeff, precision) + ")"
 
 class Exponential(Function):
     """y = a*exp(b*(x-c)) + d"""
@@ -170,12 +167,9 @@ class Exponential(Function):
     def __call__(self, x):
         return self.coeff[0]*np.exp(self.coeff[1]*(x-self.coeff[2])) + self.coeff[3]
 
-    def __str__(self):
-        return f"Exponential function with coeff {self.coeff}"
-
-    def _plot_label(self):
-        return f"{self.coeff[0]:.3f}*exp({self.coeff[1]:.3f}*(x-{self.coeff[2]:.3f}))+{self.coeff[3]:.3f}"
-
+    def __str__(self, precision=3):
+        exponent = f"{self.coeff[1]:.{precision}f}*(x-{self.coeff[2]:.{precision}f})"
+        return f"{self.coeff[0]:.{precision}f}*exp({exponent})+{self.coeff[3]:.{precision}f}"
 
 # class Exponential(Function): # y = poly(exp(poly(x)))
 # class Logarithm(Function): # y = poly(log_b(poly(x)))
