f345b7d4d80cb1489b6ec5bd3903c5e934d73a0a
Adrian Müller <dev@adrianmueller.eu>
2025-01-03 17:09:04 +0200

    quantum module is always imported

--- __init__.py
+++ __init__.py
@@ -11,7 +11,5 @@ from .examples import *
 def test_all():
     print_header('mathlib')
     test_mathlib_all()
-
     print_header('quantum')
-    from .quantum import test_quantum_all  # quantum might not be imported yet
     test_quantum_all()
