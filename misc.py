d462c1943f982d6a2a7fd36f1bb652e2342f3594
Adrian Müller <dev@adrianmueller.eu>
2025-02-17 14:00:43 +0200

    misc: Add quadratic_effect

--- misc.py
+++ misc.py
@@ -1,6 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def quadratic_effect(observations, naive_expectation_fn, n_samples=10**6):
+    """
+    Simulates how observations are biased when the probability of observing something
+    scales (linearly) with its availability in the system.
+
+    Args:
+        observations (list): numerical observations, non-negative and normalizable
+        naive_expectation_fn (function): naively expected experience with a given observation (without the effect)
+
+    The average experience will be:
+        E[availability * naive expectation] / E[availability]
+    """
+    p_observed = observations / observations.sum()
+    observed = np.random.choice(observations, size=n_samples, p=p_observed)
+    # print(f"Theoretical average: {(observations * naive_expectation_fn(observations)).mean() / observations.mean():.2f}")
+    return naive_expectation_fn(observed).mean()
+
+    # Example: Bus waiting time
+    bus_interval = 20
+    noise_std = 5
+    n_buses = 10000
+    arrival_times = np.arange(n_buses) * bus_interval + np.random.normal(0, noise_std, n_buses)
+    intervals = np.maximum(0, np.diff(np.sort(arrival_times)))
+    avg_wait = quadratic_effect(intervals, naive_expectation_fn=lambda x: x/2)  # I'd naively expect to wait on average half the regular interval
+    print(f"Average experienced wait: {avg_wait:.2f} min")
+
+    # Exmaple: Friendship paradox
+    avg_friends = 100
+    friends_friends = np.random.lognormal(mean=np.log(avg_friends) - 0.5**2/2, sigma=0.5, size=10000)
+    avg_friends_friends = quadratic_effect(friends_friends, naive_expectation_fn=lambda x: x)  # I'd naively expect my friends on average to have just as many friends as the average person
+    print(f"Average number of friends' friends: {avg_friends_friends:.2f}")
+
 def christmas_tree():
     # after https://community.wolfram.com/groups/-/m/t/175891
 
