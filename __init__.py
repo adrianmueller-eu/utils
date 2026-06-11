1b5f8a6ef1560f7ed38663150206484328b383dd
Adrian Müller <dev@adrianmueller.eu>
2026-06-12 00:54:40 +0300

    Clearer test headers

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from .examples import *
 from .misc import *
 
 def test_all():
-    print_header('mathlib')
+    print_header('utils/mathlib')
     test_mathlib_all()
-    print_header('quantum')
+    print_header('utils/quantum')
     test_quantum_all()
